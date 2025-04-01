f6a2dd147288a46acf16a58b18b57f7dea04cd74
Adam <Adambekkali2003@gmail.com>
2025-04-02 01:31:22 +0200

    add InscriptionsModule to app imports

--- synkro-backend/src/app.module.ts
+++ synkro-backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UtilisateursModule } from './utilisateurs/utilisateurs.module';
 import { EvenementsModule } from './evenements/evenements.module';
+import { InscriptionsModule } from './inscriptions/inscriptions.module';
 
 @Module({
-  imports: [PrismaModule, UtilisateursModule,EvenementsModule],
+  imports: [PrismaModule, UtilisateursModule,EvenementsModule,InscriptionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
